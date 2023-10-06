66540bb32d7a08e9e9503bd30101735955647653
KayPrime <forendals@gmail.com>
2023-10-06 18:08:57 -0400

    first commit

---
+++ math_operations.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import calculator
+
+addition = calculator.add(1,2)
+print(addition)
+
+substraction = calculator.subtract(3,4)
+print(substraction)
+
+multiplication = calculator.multiply(5,6)
+print(multiplication)
+
+division = calculator.divide(9,3)
+print(division)
